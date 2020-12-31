3c726b1e709479e0a81d1d4d66c7ed2c102eda77
fiduta <eryckson@gmail.com>
2020-12-31 15:45:32 -0300

    init

---
+++ src/Abstractions/IDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AlbedoTeam.Sdk.DataLayerAccess.Abstractions
+{
+    public interface IDbSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ src/DbSettings.cs
@@ -0,0 +1,10 @@
+﻿using AlbedoTeam.Sdk.DataLayerAccess.Abstractions;
+
+namespace AlbedoTeam.Sdk.DataLayerAccess
+{
+    public class DbSettings : IDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
